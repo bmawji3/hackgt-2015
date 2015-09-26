7709fbe18f942b45a78b28abff5a193a037f0fd2
bmawji3 <bmawji3@gatech.edu>
2015-09-26 14:32:50 -0400

    Added more pictures and set up main screen

--- 2/app/src/main/java/blueant/hackgt2015/MainActivity.java
+++ 2/app/src/main/java/blueant/hackgt2015/MainActivity.java
@@ -2,12 +2,30 @@ package blueant.hackgt2015;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
+
+    ImageView picture;
+    Button yami;
+    Button yugi;
+    Button kaiba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        picture = (ImageView) findViewById(R.id.kaiba);
+//        iv.setOnClickListener(this);
     }
+
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.
+//        }
+//    }
+
+
 }
