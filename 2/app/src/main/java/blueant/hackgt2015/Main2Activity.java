4b744fc84dea5a3c1989edea6ad5f9115093905a
bmawji3 <bmawji3@gatech.edu>
2015-09-27 02:21:22 -0400

    Added count for each servo

--- 2/app/src/main/java/blueant/hackgt2015/Main2Activity.java
+++ 2/app/src/main/java/blueant/hackgt2015/Main2Activity.java
@@ -13,7 +13,10 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,6 +48,19 @@ public class Main2Activity extends ActionBarActivity {
     private BluetoothDevice mDevice; //external BLE device (Grove BLE module)
 
     private Timer mTimer;
+    private TextView textView;
+    private TextView servo1Text;
+    private TextView servo2Text;
+    private TextView servo3Text;
+    private Button servo1plus;
+    private Button servo1minus;
+    private Button servo2plus;
+    private Button servo2minus;
+    private Button servo3plus;
+    private Button servo3minus;
+    private int count1;
+    private int count2;
+    private int count3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +69,10 @@ public class Main2Activity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mTimer = new Timer();
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hello Bluetooth LE!");
+        textView.setMovementMethod(new ScrollingMovementMethod());
 
         //check to see if Bluetooth Low Energy is supported on this device
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
@@ -82,6 +102,22 @@ public class Main2Activity extends ActionBarActivity {
             finish();
             return;
         }
+        servo1Text = (TextView) findViewById(R.id.servo1Text);
+        servo2Text = (TextView) findViewById(R.id.servo2Text);
+        servo3Text = (TextView) findViewById(R.id.servo3Text);
+
+        servo1plus = (Button) findViewById(R.id.servo1plus);
+        servo1minus = (Button) findViewById(R.id.servo1minus);
+        servo2plus = (Button) findViewById(R.id.servo2plus);
+        servo2minus = (Button) findViewById(R.id.servo2minus);
+        servo3plus = (Button) findViewById(R.id.servo3plus);
+        servo3minus = (Button) findViewById(R.id.servo3minus);
+
+        addListeners();
+
+        servo1Text.setText("" + getCount1());
+        servo2Text.setText("" + getCount2());
+        servo3Text.setText("" + getCount3());
 
         //try to find the Grove BLE V1 module
         searchForDevices();
@@ -240,7 +276,91 @@ public class Main2Activity extends ActionBarActivity {
             @Override
             public void run() {
                 Log.w("BLE", msg);
+                textView.setText(textView.getText() + "\r\n" + msg);
             }
         });
+    }
+
+    private void buttonPressed (final String msg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Button clicked", msg);
+                textView.setText(textView.getText() + "\r\n" + msg);
+            }
+        });
+    }
+
+    private void addListeners () {
+        servo1plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPressed("clicked servo1plus");
+                setCount1(1);
+                servo1Text.setText("" + getCount1());
+            }
+        });
+        servo1minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPressed("clicked servo1minus");
+                setCount1(-1);
+                servo1Text.setText("" + getCount1());
+            }
+        });
+        servo2plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPressed("clicked servo2plus");
+                setCount2(1);
+                servo2Text.setText("" + getCount2());
+            }
+        });
+        servo2minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPressed("clicked servo2minus");
+                setCount2(-1);
+                servo2Text.setText("" + getCount2());
+            }
+        });
+        servo3plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPressed("clicked servo3plus");
+                setCount3(1);
+                servo3Text.setText("" + getCount3());
+            }
+        });
+        servo3minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPressed("clicked servo3minus");
+                setCount3(-1);
+                servo3Text.setText("" + getCount3());
+            }
+        });
+    }
+
+    public int getCount1() { return count1; }
+
+    public void setCount1(int count1) {
+        this.count1 += count1;
+    }
+
+    public int getCount2() {
+        return count2;
+    }
+
+    public void setCount2(int count2) {
+        this.count2 += count2;
+    }
+
+    public int getCount3() {
+        return count3;
+    }
+
+    public void setCount3(int count3) {
+        this.count3 += count3;
     }
 }
